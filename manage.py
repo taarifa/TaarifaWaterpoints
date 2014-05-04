1983b3707be93e05216e6afddb9a06e39770e82e
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-04 18:45:51 +0100

    Add manager task to create waterpoint facility

--- manage.py
+++ manage.py
@@ -1,8 +1,22 @@
 from flask.ext.script import Manager
 
+from taarifa_api import add_document
 from taarifa_waterpoints import app
+from taarifa_waterpoints.schemas import facility_schema
 
 manager = Manager(app)
+
+
+def check(response):
+    data, _, _, status_code = response
+    print "Succeeded" if status_code == 201 else "Failed", "with", status_code
+    print data
+
+
+@manager.command
+def create_waterpoints():
+    """Create facility for waterpoints."""
+    check(add_document('facilities', facility_schema))
 
 if __name__ == "__main__":
     manager.run()
