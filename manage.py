a325b3f9b38982940bdadae849be56e512629377
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-04 18:46:53 +0100

    Add manager task to upload waterpoints from CSV file

--- manage.py
+++ manage.py
@@ -1,3 +1,7 @@
+from csv import DictReader
+from datetime import datetime
+import gzip
+
 from flask.ext.script import Manager
 
 from taarifa_api import add_document, delete_documents
@@ -17,6 +21,26 @@ def check(response):
 def create_waterpoints():
     """Create facility for waterpoints."""
     check(add_document('facilities', facility_schema))
+
+
+@manager.option("filename", help="gzipped CSV file to upload (required)")
+def upload_waterpoints(filename):
+    """Upload waterpoints from a gzipped CSV file."""
+    convert = {
+        'date_recorded': lambda s: datetime.strptime(s, '%m/%d/%Y'),
+        'population': int,
+        'construction_year': lambda s: datetime.strptime(s, '%Y'),
+        'breakdown_year': lambda s: datetime.strptime(s, '%Y'),
+        'amount_tsh': float,
+        'gps_height': float,
+        'latitude': float,
+        'longitude': float,
+    }
+    with gzip.open(filename) as f:
+        for d in DictReader(f):
+            d = dict((k, convert.get(k, str)(v)) for k, v in d.items() if v)
+            d['facility_code'] = 'wp001'
+            check(add_document('waterpoints', d))
 
 
 @manager.command
