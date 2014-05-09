28ae8db1d0112b0a14185914fe94c50a451ac2ea
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-10 00:38:25 +0100

    Add manage task to show a given schema

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from csv import DictReader
 from datetime import datetime
 import gzip
+from pprint import pprint
 
 from flask.ext.script import Manager
 
-from taarifa_api import add_document, delete_documents
+from taarifa_api import add_document, delete_documents, get_schema
 from taarifa_waterpoints import app
 from taarifa_waterpoints.schemas import facility_schema
 
@@ -15,6 +16,12 @@ def check(response):
     data, _, _, status_code = response
     print "Succeeded" if status_code == 201 else "Failed", "with", status_code
     print data
+
+
+@manager.option("resource", help="Resource to show the schema for")
+def show_schema(resource):
+    """Show the schema for a given resource."""
+    pprint(get_schema(resource))
 
 
 @manager.command
