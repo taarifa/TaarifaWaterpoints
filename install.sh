b5a9de073939cc3ffa26cbc7f38fb67a539c5c9e
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-24 08:21:44 +0100

    Add script to install system-wide packages

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y git python-pip python-setuptools mongodb nodejs
+sudo pip install virtualenv virtualenvwrapper
+
+if ! grep -q WORKON_HOME $HOME/.profile; then
+  echo "export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME
+source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.profile
+fi
