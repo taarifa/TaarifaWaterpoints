cf993524f8ffd4f9297212c2f857cff7a1b2fc0a
Dirk Gorissen <dgorissen@gmail.com>
2014-06-24 15:38:15 +0100

    Increase default eve pagination size

--- taarifa_waterpoints/taarifa_waterpoints.py
+++ taarifa_waterpoints/taarifa_waterpoints.py
@@ -5,6 +5,12 @@ from taarifa_api import api as app, main
 
 app.name = 'TaarifaWaterpoints'
 
+# Override the maximum number of results on a single page
+# This is needed by the dashboard
+# FIXME: this should eventually be replaced by an incremental load
+# which is better for responsiveness
+app.config['PAGINATION_LIMIT'] = 10000
+
 
 @app.route('/' + app.config['URL_PREFIX'] + '/waterpoints/values/<field>')
 def waterpoint_values(field):
