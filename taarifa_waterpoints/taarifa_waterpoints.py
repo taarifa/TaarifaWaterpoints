8c92c204b3a057c58d1f1644d72e3b68bac1b4d3
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-09-27 09:44:24 +0100

    Fix directory to serve topojson from

--- taarifa_waterpoints/taarifa_waterpoints.py
+++ taarifa_waterpoints/taarifa_waterpoints.py
@@ -164,7 +164,7 @@ def images(filename):
 
 @app.route('/data/<path:filename>.topojson')
 def geojson(filename):
-    return send_from_directory(app.root_path + '/app/data/', filename + '.topojson', mimetype=mimetypes.types_map['.json'])
+    return send_from_directory(app.root_path + '/dist/data/', filename + '.topojson', mimetype=mimetypes.types_map['.json'])
 
 
 @app.route('/data/<path:filename>')
