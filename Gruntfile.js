34cbd117e4df89d4033bb9bc62be7a3c7d61176e
Florian Rathgeber <florian.rathgeber@gmail.com>
2020-10-18 20:35:12 +0200

    Remove ng-annotate

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,20 +270,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngAnnotate tries to make the code safe for minification automatically by
-    // using the Angular long form for dependency injection. It doesn't work on
-    // things like resolve or inject so those have to be done manually.
-    ngAnnotate: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/concat/scripts',
-          src: '*.js',
-          dest: '.tmp/concat/scripts'
-        }]
-      }
-    },
-
     // Replace Google CDN references
     cdnify: {
       dist: {
@@ -437,7 +423,6 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngAnnotate',
     'copy:dist',
     'cssmin',
     'uglify',
